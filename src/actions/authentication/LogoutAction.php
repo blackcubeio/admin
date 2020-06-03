ed174f04981e3106507823026a17cce52d62345c
Philippe Gaultier <pgaultier@ibitux.com>
2020-06-03 18:46:40 +0200

    Modularize authentication

---
+++ src/actions/authentication/LogoutAction.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * LogoutAction.php
+ *
+ * PHP version 7.2+
+ *
+ * @author Philippe Gaultier <pgaultier@redcat.io>
+ * @copyright 2010-2020 Redcat
+ * @license https://www.redcat.io/license license
+ * @version XXX
+ * @link https://www.redcat.io
+ * @package blackcube\admin\actions\authentication
+ */
+
+namespace blackcube\admin\actions\authentication;
+
+use yii\base\Action;
+use Yii;
+
+/**
+ * Class LogoutAction
+ *
+ * @author Philippe Gaultier <pgaultier@redcat.io>
+ * @copyright 2010-2020 Redcat
+ * @license https://www.redcat.io/license license
+ * @version XXX
+ * @link https://www.redcat.io
+ * @package blackcube\admin\actions\authentication
+ */
+class LogoutAction extends Action
+{
+    /**
+     * @var string where to redirect
+     */
+    public $targetAction = 'login';
+
+    /**
+     * @return string|\yii\web\Response
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function run()
+    {
+        Yii::$app->user->logout();
+        return $this->controller->redirect([$this->targetAction]);
+    }
+}
