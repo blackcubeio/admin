1264778433f26f7fd26a2c88e670b6c57e738e92
Philippe Gaultier <pgaultier@ibitux.com>
2020-06-23 14:39:40 +0200

    Fixing missing use

--- src/actions/category/CreateAction.php
+++ src/actions/category/CreateAction.php
@@ -17,6 +17,7 @@ namespace blackcube\admin\actions\category;
 use blackcube\admin\actions\BaseElementAction;
 use blackcube\admin\helpers\Category as CategoryHelper;
 use blackcube\admin\models\SlugForm;
+use blackcube\admin\Module;
 use blackcube\core\interfaces\PluginHookInterface;
 use blackcube\core\interfaces\PluginsHandlerInterface;
 use blackcube\core\models\Category;
