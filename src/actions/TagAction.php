2e5e925a7834abf5d2ff6e46a39c58c4fbc5997e
Philippe Gaultier <pgaultier@redcat.io>
2022-04-17 17:52:04 +0200

    change copyright

--- src/actions/TagAction.php
+++ src/actions/TagAction.php
@@ -5,7 +5,7 @@
  * PHP version 7.2+
  *
  * @author Philippe Gaultier <pgaultier@redcat.io>
- * @copyright 2010-2020 Redcat
+ * @copyright 2010-2022 Redcat
  * @license https://www.redcat.io/license license
  * @version XXX
  * @link https://www.redcat.io
@@ -34,7 +34,7 @@ use Yii;
  * Class TagAction
  *
  * @author Philippe Gaultier <pgaultier@redcat.io>
- * @copyright 2010-2020 Redcat
+ * @copyright 2010-2022 Redcat
  * @license https://www.redcat.io/license license
  * @version XXX
  * @link https://www.redcat.io
